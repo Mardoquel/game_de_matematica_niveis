e816bdfabcef37f2b40a75b08a3e37092d43ccc0
Mardoquel Gadi <71660451+mardoca@users.noreply.github.com>
2021-01-26 17:34:08 -0300

    Add files via upload

---
+++ Game/game.py
@@ -0,0 +1,34 @@
+from models.calcular import Calcular
+
+
+def main() -> None:
+    pontos: int = 0
+    jogar(pontos)
+
+
+def jogar(pontos: int) -> None:
+    dificuldade: int = int(input('Informe o nivel de dificulade desejado [1, 2, 3 ou 4]: '))
+
+    calc: Calcular = Calcular(dificuldade)
+
+    print('Informe o resultado para a seguinte operacacao: ')
+    calc.mostrar_operacao()
+
+    resultado: int = int(input())
+
+    if calc.checar_resultado(resultado):
+        pontos += 1
+        print(f'\nVoce tem {pontos} pontos(s).')
+
+    continuar: int = int(input('Deseja continuar no jogo? [1 - sim, 0 - nao] '))
+
+    if continuar:
+        jogar(pontos)
+    else:
+        print(f'\nVoce finalizou com {pontos} ponto(s).')
+        print('Ate a proxima!')
+
+
+if __name__ == '__main__':
+    main()
+
